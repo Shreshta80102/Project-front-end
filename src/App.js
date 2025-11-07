636b7cde21380626100a000998eb3d7986860805
Shreshta80102 <kakaralashreshta@gmail.com>
2025-11-07 10:34:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React from 'react';
-import Header from './Header';
-import Home from './Home';
-import Journeys from './Journeys';
+import Header from './components/Header';
+import Home from './components/Home';
+import Journeys from './components/Journeys';
+import './App.css';
 
 function App() {
-  const appStyles = {
-    fontFamily: 'Nunito, Lato, system-ui, -apple-system, "Segoe UI", Roboto, Arial, sans-serif',
-    color: '#4b3a2a',
-    backgroundColor: '#fff',
-    margin: 0,
-    padding: 0,
-  };
-  const mainStyles = {
-    maxWidth: 1100,
-    margin: '0 auto',
-    padding: '0 24px 64px',
-  };
   return (
-    <div style={appStyles}>
+    <div className="App">
       <Header />
-      <main style={mainStyles}>
-        <section id="home">
-          <Home />
-        </section>
-        <section id="journeys" style={{ scrollMarginTop: 88 }}>
-          <Journeys />
-        </section>
-      </main>
+      <Home />
+      <Journeys />
     </div>
   );
 }
+
 export default App;
+
